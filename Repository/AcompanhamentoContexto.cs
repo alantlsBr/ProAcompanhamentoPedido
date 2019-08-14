267f7493b56fbd8c422bed18e7e664a337fbf597
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-13 21:05:29 -0400

    Adicionado classe contexto

---
+++ Repository/AcompanhamentoContexto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAcompanhamentoPedido.Dominio;
+using ProAcompanhamentoPedido.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAcompanhamentoPedido.Repository
+{
+    public class AcompanhamentoContexto : DbContext
+    {
+        public AcompanhamentoContexto(DbContextOptions<AcompanhamentoContexto> opcoes) : base(opcoes)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Schema do Banco de dados
+            modelBuilder.HasDefaultSchema("public");
+
+            //Mapeamento Relacionamento
+            modelBuilder.Entity<Pedido>()
+                .HasOne(p => p.Cliente)
+                .WithMany(c => c.Pedidos)
+                .HasForeignKey(p => p.ClienteId);
+
+            //Mapeamento do enum DomStatusPedido
+            modelBuilder
+                .Entity<Pedido>()
+                .Property(p => p.Status)
+                .HasConversion(
+                v => v.ToString(),
+                v => (DomStatusPedido)Enum.Parse(typeof(DomStatusPedido), v));
+
+        }
+    }
+}
